2a6ba438aa032e067d7bd5a3d629be5a40cd0925
EsterbauerM <backfischliebtfisch@gmail.com>
2021-12-07 16:44:56 +0100

    ya yeet

---
+++ function.js
@@ -0,0 +1,20 @@
+window.onload = function (){
+
+    document.getElementById ("previous").onclick = previousImg;
+
+	document.getElementById("slideshow").onclick = slideshow;
+
+    document.getElementById("next").onclick = nextImg;
+}
+
+function previousImg(){
+
+}
+
+function slideshow(){
+
+}
+
+function nextImg(){
+    
+}
